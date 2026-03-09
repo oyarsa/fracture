6468f38d9ba316194b2c83adca658263cfadf827
Italo Silva <italo@maleldil.com>
2026-03-09 02:03:55 +0000

    Merge curve/curve2 and powerlaw/powerlaw2 with --mode switches

--- curve.py
+++ curve.py
@@ -5,18 +5,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 opt = argparse.ArgumentParser(
-    description='Evaluate fracture power law.',
+    description='Plot fracture V-I curves.',
     formatter_class=argparse.ArgumentDefaultsHelpFormatter
 )
 
 opt.add_argument(
     '-s', '--source',
-    type=str
+    type=str,
+    required=True
 )
 
 opt.add_argument(
-	'-d', '--disorder',
-	type=float
+    '-m', '--mode',
+    type=str,
+    choices=['size', 'disorder'],
+    required=True,
+    help='Vary over lattice size or disorder'
 )
 
 opt.add_argument(
@@ -25,35 +29,71 @@ opt.add_argument(
     choices=['t', 'h', 's']
 )
 
+opt.add_argument(
+    '-d', '--disorder',
+    type=float,
+    help='Fixed disorder value (required for mode=size)'
+)
+
+opt.add_argument(
+    '-l', '--length',
+    type=int,
+    help='Fixed length value (required for mode=disorder)'
+)
+
+
 def convert_geometry(initial):
-	return {
-		't': '45 graus',
-		'h': 'hexagonal',
-		's': 'quadrada'
-	}[initial]
+    return {
+        't': '45 graus',
+        'h': 'hexagonal',
+        's': 'quadrada'
+    }[initial]
 
 
-def sort_files(path):
+def sort_by_size(path):
     filename = os.path.basename(path)
     length = filename.split('.')[0]
     return int(length)
 
 
-args = opt.parse_args()
-files = sorted(glob.glob(args.source + "/*.csv"), key=sort_files)
-sizes = [7, 14, 20, 28]
-data = [np.genfromtxt(path, delimiter=',', skip_header=1, names=['V', 'I'])
-        for path in files]
+def sort_by_disorder(path):
+    filename = os.path.basename(path)
+    disorder = filename.split('.')[2]
+    disorder = disorder.replace(',', '.')
+    return float(disorder)
 
-for L, data in zip(sizes, data):
-    plt.plot(data['V'], data['I'], label=f'L = {L}')
+
+args = opt.parse_args()
+
+if args.mode == 'size':
+    files = sorted(glob.glob(args.source + "/*.csv"), key=sort_by_size)
+    sizes = [7, 14, 20, 28]
+    data = [np.genfromtxt(path, delimiter=',', skip_header=1, names=['V', 'I'])
+            for path in files]
+
+    for L, d in zip(sizes, data):
+        plt.plot(d['V'], d['I'], label=f'L = {L}')
+
+    disorder = args.disorder
+    geometry = convert_geometry(args.geometry)
+    plt.title(f'$D = {disorder}, G = ${geometry}')
+
+else:
+    files = sorted(glob.glob(args.source + "/*.csv"), key=sort_by_disorder)
+    disorders = [0, 0.2, 0.4, 0.6, 0.8, 1.0]
+    data = [np.genfromtxt(path, delimiter=',', skip_header=1, names=['V', 'I'])
+            for path in files]
+
+    for D, d in zip(disorders, data):
+        plt.plot(d['V'], d['I'], label=f'D = {D}')
+
+    length = args.length
+    geometry = convert_geometry(args.geometry)
+    plt.title(f'$L = {length}$, G = {geometry}')
 
 plt.legend(loc='upper left')
 plt.grid(True)
 plt.axis('equal')
 plt.xlabel(r'$V$')
 plt.ylabel(r'$I$')
-disorder = args.disorder
-geometry = convert_geometry(args.geometry)
-plt.title(f'$D = {disorder}, G = ${geometry}')
 plt.show()
--- powerlaw.py
+++ powerlaw.py
@@ -11,47 +11,121 @@ opt = argparse.ArgumentParser(
 
 opt.add_argument(
     '-s', '--source',
-    type=str
+    type=str,
+    required=True
 )
 
-def fit(data, alfa, beta, L):
+opt.add_argument(
+    '-m', '--mode',
+    type=str,
+    choices=['size', 'disorder'],
+    required=True,
+    help='Vary over lattice size or disorder'
+)
+
+opt.add_argument(
+    '-g', '--geometry',
+    type=str,
+    choices=['t', 'h', 's'],
+    help='Geometry type (required for mode=disorder)'
+)
+
+opt.add_argument(
+    '-l', '--length',
+    type=int,
+    help='Fixed length value (required for mode=disorder)'
+)
+
+
+def convert_geometry(initial):
+    return {
+        't': '45 graus',
+        'h': 'hexagonal',
+        's': 'quadrada'
+    }[initial]
+
+
+def fit_size(data, alfa, beta, L):
     V = data['V']
     I = data['I']
-
     vlb = V / (L**beta)
     ilb = I / (L**alfa)
-
     return ilb, vlb
 
 
-def sort_files(path):
+def fit_disorder(data, eta, nu, D):
+    V = data['V']
+    I = data['I']
+    vlb = V * (1 + D)**nu
+    ilb = I * (1 + D)**eta
+    return ilb, vlb
+
+
+def sort_by_size(path):
     filename = os.path.basename(path)
     length = filename.split('.')[0]
     return int(length)
 
 
+def sort_by_disorder(path):
+    filename = os.path.basename(path)
+    disorder = filename.split('.')[2]
+    disorder = disorder.replace(',', '.')
+    return float(disorder)
+
+
 args = opt.parse_args()
-files = sorted(glob.glob(args.source + "/*.csv"), key=sort_files)
-print('\n'.join(files))
-sizes = [7, 14, 20, 28]
-data = [np.genfromtxt(path, delimiter=',', skip_header=1, names=['V', 'I'])
-        for path in files]
 
-while True:
-    try:
-        alfa = float(input('\nalfa: '))
-        beta = float(input('beta: '))
-    except ValueError:
-        break
-    fitresult = [(L, fit(d, alfa, beta, L)) for L, d in zip(sizes, data)]
+if args.mode == 'size':
+    files = sorted(glob.glob(args.source + "/*.csv"), key=sort_by_size)
+    print('\n'.join(files))
+    sizes = [7, 14, 20, 28]
+    data = [np.genfromtxt(path, delimiter=',', skip_header=1, names=['V', 'I'])
+            for path in files]
 
-    for L, (ilb, vlb) in fitresult:
-        plt.plot(vlb, ilb, label=f'L = {L}')
+    while True:
+        try:
+            alfa = float(input('\nalfa: '))
+            beta = float(input('beta: '))
+        except ValueError:
+            break
+        fitresult = [(L, fit_size(d, alfa, beta, L)) for L, d in zip(sizes, data)]
 
-    plt.legend(loc='upper left')
-    plt.grid(True)
-    plt.axis('equal')
-    plt.xlabel(r'$V/L^\beta$')
-    plt.ylabel(r'$I/L^\alpha$')
-    plt.title(f'$D = 0$, $\\alpha = {alfa}$ e $\\beta = {beta}$')
-    plt.show()
+        for L, (ilb, vlb) in fitresult:
+            plt.plot(vlb, ilb, label=f'L = {L}')
+
+        plt.legend(loc='upper left')
+        plt.grid(True)
+        plt.axis('equal')
+        plt.xlabel(r'$V/L^\beta$')
+        plt.ylabel(r'$I/L^\alpha$')
+        plt.title(f'$D = 0$, $\\alpha = {alfa}$ e $\\beta = {beta}$')
+        plt.show()
+
+else:
+    files = sorted(glob.glob(args.source + "/*.csv"), key=sort_by_disorder)
+    print('\n'.join(files))
+    disorders = [0, 0.2, 0.4, 0.6, 0.8, 1.0]
+    data = [np.genfromtxt(path, delimiter=',', skip_header=1, names=['V', 'I'])
+            for path in files]
+
+    while True:
+        try:
+            eta = float(input('\neta: '))
+            nu = float(input('nu: '))
+        except ValueError:
+            break
+        fitresult = [(D, fit_disorder(d, eta, nu, D)) for D, d in zip(disorders, data)]
+
+        for D, (ilb, vlb) in fitresult:
+            plt.plot(vlb, ilb, label=f'D = {D}')
+
+        plt.legend(loc='upper left')
+        plt.grid(True)
+        plt.xlabel(r'$V(1+D)^{\nu}$')
+        plt.ylabel(r'$I(1+D)^{\eta}$')
+
+        length = args.length
+        geometry = convert_geometry(args.geometry)
+        plt.title(f'$L = {length}$, G = {geometry}, $\\eta = {eta}$ e $\\nu = {nu}$')
+        plt.show()
